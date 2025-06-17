42280a0a3a4d1ca3430afb118f1abc122288c5ee
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-06-17 14:12:55 -0300

    Create project and Header

---
+++ src/componentes/Header/index.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import TextField from '../TextField';
+
+const StyledHeader = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 20px;
+    img{
+        max-width: 212px;
+    }
+`
+
+export default function Header() {
+    return(
+        <StyledHeader>
+            <img src="/public/imagens/logo.png" alt="Logo Space-App" />
+            <TextField />
+        </StyledHeader>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import GlobalStyle from './componentes/GlobalStyle'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GlobalStyle />
+    <App />
+  </StrictMode>,
+)
